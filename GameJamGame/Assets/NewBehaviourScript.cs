af4ac9785db849e29dfd090445ed15b772b10de2
Yasin Zuh <yasinzuh@gmail.com>
2016-10-14 21:04:00 -0700

    Added script for key D

---
+++ GameJamGame/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+            transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+        }
+	}
+}

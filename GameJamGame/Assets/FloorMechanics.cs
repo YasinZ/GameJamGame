ad8d165a9d00659bc1b7949b9fec1675e61bbcd2
Yasin Zuh <yasinzuh@gmail.com>
2016-10-15 15:13:47 -0700

    Floor semiworking

---
+++ GameJamGame/Assets/FloorMechanics.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorMechanics : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.color = new Color(0, 0, 0, 1f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //GetComponent<Renderer>().material.color += new Color(0, 0, 0, .1f);
+        //GetComponent<Renderer>().material.color = new Color(0, 0, 0, 1f);
+    }
+
+    void OnTriggerEnter2D(Collider2D c1)
+    {
+        GetComponent<Renderer>().material.color = new Color(0, 0, 0, -1f);
+    }
+
+    void PolygonColliderExit2D(Collider2D c1)
+    {
+        GetComponent<Renderer>().material.color = new Color(0, 0, 0, 1f);
+    }
+
+    void OnTriggerExit2D(Collider2D c1)
+    {
+        GetComponent<Renderer>().material.color = new Color(0, 0, 0, 1f);
+    }
+}

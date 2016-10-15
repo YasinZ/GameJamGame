9eea0f07c7e2170038fdf01f5bb8545a66f1bdc5
Yasin Zuh <yasinzuh@gmail.com>
2016-10-15 12:16:32 -0700

    Grounded function working, only able to jump once

--- GameJamGame/Assets/PlayerMovement.cs
+++ GameJamGame/Assets/PlayerMovement.cs
@@ -22,10 +22,38 @@ public class PlayerMovement : MonoBehaviour {
 
         rigid.velocity = v2;
 
-        if(Input.GetKeyDown(KeyCode.Space) ) 
+        if(Input.GetKeyDown(KeyCode.Space) && Grounded) 
         {
             v2.y = 5;
             rigid.velocity = v2;
         }
 	}
+
+    bool Grounded;
+
+    void OnCollisionStay2D(Collision2D collider)
+    {
+        CheckIfGrounded();
+    }
+
+    void OnCollisionExit2D(Collision2D collider)
+    {
+        Grounded = false;
+    }
+
+    private void CheckIfGrounded()
+    {
+        RaycastHit2D[] hits;
+
+        //We raycast down 1 pixel from this position to check for a collider
+        Vector2 positionToCheck = transform.position;
+        hits = Physics2D.RaycastAll(positionToCheck, new Vector2(0, -1), 0.01f);
+
+        //if a collider was hit, we are grounded
+        if (hits.Length > 0)
+        {
+            Grounded = true;
+        }
+    }
+
 }

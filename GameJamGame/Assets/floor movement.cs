4430e12395b6beb0b19429d34cb00e321fcf7835
Kentster82 <kent.pluntze@gmail.com>
2016-10-15 16:53:14 -0700

    Platform
    start of platform movement.

---
+++ GameJamGame/Assets/floor movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class floormovement : MonoBehaviour {
+    float x;
+    bool left = false;
+    // Use this for initialization
+    void Start () {
+        float x = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (left)
+        {
+            x -= .1F;
+        }
+        else
+        {
+            x += .1F;
+        }
+            if (x>=10 || x <= 0)
+        {
+            left = !left;
+        }
+        
+    }
+}

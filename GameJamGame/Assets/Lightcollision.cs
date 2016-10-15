93e15532a608dd030d85817236b8b46731e683a0
Kentster82 <kent.pluntze@gmail.com>
2016-10-15 14:48:37 -0700

    fading
    got the alpha changes working

--- GameJamGame/Assets/Lightcollision.cs
+++ GameJamGame/Assets/Lightcollision.cs
@@ -2,20 +2,24 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class Lightcollision : MonoBehaviour {
+public class Lightcollision : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-        Sprite Light= new Sprite();
+    // Use this for initialization
+    void Start()
+    {
+        Sprite Light = new Sprite();
         Debug.Log(Light.associatedAlphaSplitTexture);
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Renderer>().material.color -= new Color(0, 0, 0, .01f);
+    }
     void OnTriggerEnter2D(Collider2D c1)
     {
-        SceneManager.LoadScene("New scene");
+        GetComponent<Renderer>().material.color += new Color(0, 0, 0, 1f);
     }
 }
+    
